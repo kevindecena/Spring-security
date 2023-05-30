7bbf8318d882bef0c3dce3f46acf6e9fee6d31dc
Kevin Decena <kcdecena.16@gmail.com>
2023-05-30 13:25:58 +0800

    added jdbc and h2

--- src/main/java/com/springsecurity/springsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/springsecurity/springsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,12 +2,17 @@ package com.springsecurity.springsecurity.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -50,4 +55,13 @@ public class BasicAuthSecurityConfiguration {
         return new InMemoryUserDetailsManager(user, admin);
     }
 
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
 }
+
+
