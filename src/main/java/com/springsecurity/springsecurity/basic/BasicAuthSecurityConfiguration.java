d89f5cbdb0c8f43b5150f9cf8b27f955f0cfaee9
Kevin Decena <kcdecena.16@gmail.com>
2023-05-30 14:14:13 +0800

    added password hashing

--- src/main/java/com/springsecurity/springsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/springsecurity/springsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -67,12 +68,16 @@ public class BasicAuthSecurityConfiguration {
     public UserDetailsService userDetailsService(DataSource dataSource) {
 
         var user = User.withUsername("in28minutes")
-                .password("{noop}dummy")
+                //.password("{noop}dummy")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
                 .roles("USER")
                 .build();
 
         var admin = User.withUsername("admin")
-                .password("{noop}dummy")
+                //.password("{noop}dummy")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
                 .roles("ADMIN")
                 .build();
 
@@ -82,6 +87,11 @@ public class BasicAuthSecurityConfiguration {
 
 
         return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
